b201b1ca58d2034eb98a6c3ae6dc260853b09899
Hedayatollah Haddadi <133634959+HedayatHaddadi@users.noreply.github.com>
2025-02-03 12:58:57 +0330

    the most recent files

--- step4_missing_smiles.py
+++ step4_missing_smiles.py
@@ -14,7 +14,8 @@ def fetch_entry_data(nan_rows_id):
     entry_data = []
     for id in tqdm(nan_rows_id, desc="Fetching ILThermo Data", unit="id"):
         entry = ilt.GetEntry(id)
-        cmp1, cmp2 = entry.components[0], entry.components[1]
+        cmp1 = entry.components[0]
+        cmp2 = entry.components[1]
         entry_data.append([
             id, 
             cmp1.id, cmp1.name, cmp1.formula, cmp1.smiles, cmp1.smiles_error, cmp1.sample, cmp1.mw,
@@ -22,26 +23,24 @@ def fetch_entry_data(nan_rows_id):
         ])
     return entry_data
 
-def create_dataframe(entry_data):
+def create_smiles_resolved_df(entry_data):
     columns = [
         "id", "cmp1_id", "cmp1_name", "cmp1_formula", "cmp1_smiles", "cmp1_smiles_error", "cmp1_sample", "cmp1_mw",
         "cmp2_id", "cmp2_name", "cmp2_formula", "cmp2_smiles", "cmp2_smiles_error", "cmp2_sample", "cmp2_mw"
     ]
-    return pd.DataFrame(entry_data, columns=columns), columns  # Return both DataFrame and columns list
+    return pd.DataFrame(entry_data, columns=columns)
 
-def merge_data(df, smiles_resolved, columns):
+def merge_and_fill_data(df, smiles_resolved):
+    missing_columns = [
+        "cmp1_id", "cmp1_name", "cmp1_formula", "cmp1_smiles", "cmp1_smiles_error", "cmp1_sample", "cmp1_mw",
+        "cmp2_id", "cmp2_name", "cmp2_formula", "cmp2_smiles", "cmp2_smiles_error", "cmp2_sample", "cmp2_mw"
+    ]
     df_filled = df.merge(smiles_resolved, on='id', how='left', suffixes=('', '_resolved'))
-    
-    for column in columns[1:]:  # Skip "id" since it is not missing
-        df_filled[column] = df_filled[column].fillna(df_filled[column + '_resolved']).infer_objects(copy=False)
-    
+    for column in missing_columns:
+        if column + '_resolved' in df_filled.columns:
+            df_filled[column] = df_filled[column].fillna(df_filled[column + '_resolved'])
     df_filled.drop(columns=[col for col in df_filled if col.endswith('_resolved')], inplace=True)
-    
-    # Explicitly infer object types to avoid future downcasting issues
-    df_filled = df_filled.infer_objects(copy=False)
-    
     return df_filled
-
 
 def save_data(df, file_path):
     df.to_csv(file_path, index=False)
@@ -51,8 +50,8 @@ def main():
     df = read_data(file_path)
     nan_rows_id = filter_nan_rows(df)
     entry_data = fetch_entry_data(nan_rows_id)
-    smiles_resolved, columns = create_dataframe(entry_data)  # Get both DataFrame and columns list
-    df_filled = merge_data(df, smiles_resolved, columns)
+    smiles_resolved = create_smiles_resolved_df(entry_data)
+    df_filled = merge_and_fill_data(df, smiles_resolved)
     save_data(df_filled, 'updated_activity_data_filled.csv')
 
 if __name__ == "__main__":
