f07c28a2b66ff626e1b32c2d670adc7e3661ee73
Hedayatollah Haddadi <133634959+HedayatHaddadi@users.noreply.github.com>
2025-02-07 12:52:25 +0330

    change name seudo_group to group_6

--- fun_2.py
+++ fun_2.py
@@ -35,5 +35,13 @@ for col in processed_df.columns:
     if 'temperature_group_' in col or col == 'temperature_seudo_group':
         processed_df[f'inv_{col}'] = processed_df[col].apply(lambda x: [1/float(i) for i in json.loads(x)] if isinstance(x, str) else x)
 
+# Rename seudo_group columns to the last group name
+group_numbers = [int(col.split('_')[-1]) for col in processed_df.columns if col.split('_')[-1].isdigit()]
+last_group_number = max(group_numbers) if group_numbers else 0
+for col in processed_df.columns:
+    if 'seudo_group' in col:
+        new_col_name = col.replace('seudo_group', f'group_{last_group_number + 1}')
+        processed_df.rename(columns={col: new_col_name}, inplace=True)
+
 # Save the filtered DataFrame (optional)
 processed_df.to_csv('filtered_grouped_data.csv', index=False)
